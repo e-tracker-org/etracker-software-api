35004ea5f8fe467685ff93561b58804fee7b6080
Oyeniji <nijoskysadeeq@gmail.com>
2024-03-18 14:40:31 +0100

    Manually added category to transaction model

--- server/src/modules/transaction/transaction.controller.ts
+++ server/src/modules/transaction/transaction.controller.ts
@@ -1,92 +1,101 @@
+import { NextFunction, Request, Response } from 'express';
+import { ReceiptBody } from '../receipt/receipt.schema';
+import { findUserByEmail } from '../auth/register/register.service';
+import { createReceiptCategory, findReceiptByCategory, findReceiptById } from '../receipt/receipt.service';
+import { apiResponse } from '../../utils/response';
+import { transactionBody } from './transaction.schema';
+import { createTransaction, findTransaction, updateTransaction } from './transaction.service';
+import { findById } from '../profile/profile.service';
+import { genarateUniqueId as generateTransactionId } from '../../utils/generateUniqueId';
+import { number } from 'zod';
 
-import {NextFunction, Request, Response} from "express";
-import {ReceiptBody} from "../receipt/receipt.schema";
-import {findUserByEmail} from "../auth/register/register.service";
-import {createReceiptCategory, findReceiptByCategory, findReceiptById} from "../receipt/receipt.service";
-import {apiResponse} from "../../utils/response";
-import {transactionBody} from "./transaction.schema";
-import {
-    createTransaction,
-    findTransaction,
-    updateTransaction
-} from "./transaction.service";
-import {findById} from "../profile/profile.service";
-import {genarateUniqueId as generateTransactionId} from "../../utils/generateUniqueId";
-import {number} from "zod";
+export async function createTransactionHandler(
+  req: Request<{}, {}, transactionBody>,
+  res: Response,
+  next: NextFunction
+) {
+  const { category, dueDate, amount, tenants } = req.body;
+  const { email } = res.locals.user;
+  try {
+    //Confirm logged in user exist
+    const user = await findUserByEmail(email);
+    if (!user) throw 'User not found';
 
-export async function createTransactionHandler(req: Request<{}, {}, transactionBody>, res: Response, next: NextFunction) {
-    const { category, dueDate, amount, tenants } = req.body;
-    const { email } = res.locals.user;
-    try {
-        //Confirm logged in user exist
-        const user = await findUserByEmail(email);
-        if(!user) throw 'User not found';
+    //Validate all request parameters
+    if (!category) throw 'Transaction category is required';
+    if (!dueDate) throw 'Transaction due date is required';
+    if (!amount) throw 'Transaction amount is required';
+    if (!tenants) throw 'Transaction receipients  required';
 
-        //Validate all request parameters
-        if(!category) throw 'Transaction category is required';
-        if(!dueDate) throw 'Transaction due date is required';
-        if(!amount) throw 'Transaction amount is required';
-        if(!tenants) throw 'Transaction receipients  required';
+    // const categoryExist = await findReceiptById(category);
+    // if(!categoryExist) throw 'Receipt category does not exist';
 
-        const categoryExist = await findReceiptById(category);
-        if(!categoryExist) throw 'Receipt category does not exist';
+    const categoryDescriptions: Record<string, string> = {
+      rent: 'Monthly rent payment',
+      'light bill': 'Electricity bill payment',
+      'water bill': 'Water bill payment',
+      'cleaner bill': 'Payment for cleaning services',
+    };
 
-        const transactionInfo: any = req.body;
-        transactionInfo.created_by = user.id;
-        const recipients = []
+    const lowercaseCategory = category.toLowerCase();
+    const predefinedCategories = Object.keys(categoryDescriptions).map((cat) => cat.toLowerCase());
 
-        for (const tenantId of tenants) { // array of tenant's or users id
-            //create each transaction for each tenant
-            delete transactionInfo.tenants
-            transactionInfo.received_by = tenantId;
-            transactionInfo.status = 'PAID'
-            const receipt = await createTransaction(transactionInfo);
-            //set up receipients email to send receipt to
-            const tenantInfo = await findById(tenantId);
-            recipients.push({id: tenantInfo.id, email: tenantInfo.email});
-
-        }
-        transactionInfo.description = categoryExist.description
-        transactionInfo.category = categoryExist.name
-        transactionInfo.recipients = recipients
-        res.locals.transactionInfo = transactionInfo;
-        next();
-    } catch (err) {
-        next(err)
+    if (!predefinedCategories.includes(lowercaseCategory)) {
+      throw 'Invalid transaction category';
     }
+
+    const transactionInfo: any = req.body;
+    transactionInfo.created_by = user.id;
+    const recipients = [];
+
+    for (const tenantId of tenants) {
+      // array of tenant's or users id
+      //create each transaction for each tenant
+      delete transactionInfo.tenants;
+      transactionInfo.received_by = tenantId;
+      transactionInfo.status = 'PAID';
+      const receipt = await createTransaction(transactionInfo);
+      //set up receipients email to send receipt to
+      const tenantInfo = await findById(tenantId);
+      recipients.push({ id: tenantInfo.id, email: tenantInfo.email });
+    }
+    transactionInfo.description = categoryDescriptions[lowercaseCategory];
+    transactionInfo.category = category;
+    transactionInfo.recipients = recipients;
+    res.locals.transactionInfo = transactionInfo;
+    next();
+  } catch (err) {
+    next(err);
+  }
 }
 
-
-
 export async function updateTransactionHandler(req: Request<{}, {}, {}>, res: Response, next: NextFunction) {
-   const {receiptURL, recipients } =  res.locals.updateTransactionInfo
+  const { receiptURL, recipients } = res.locals.updateTransactionInfo;
 
-
-    for (const recipient of recipients) {
-        const transactionId = generateTransactionId();
-        await updateTransaction(recipient.id,  {transactionId, receiptFile: receiptURL});
-    }
-    return apiResponse(res, 'Transaction created and receipt email sent successfully', '', 201);
+  for (const recipient of recipients) {
+    const transactionId = generateTransactionId();
+    await updateTransaction(recipient.id, { transactionId, receiptFile: receiptURL });
+  }
+  return apiResponse(res, 'Transaction created and receipt email sent successfully', '', 201);
 }
 
 export async function findTransanctionHandler(req: Request<{}, {}, {}>, res: Response, next: NextFunction) {
-    const { email } = res.locals.user;
-    const { accountType } = req.params;
+  const { email } = res.locals.user;
+  const { accountType } = req.params;
 
-    if (!accountType) throw 'Account type required';
+  if (!accountType) throw 'Account type required';
 
-    try {
-        //Confirm logged in user exist
-        const user = await findUserByEmail(email);
-        if (!user) throw 'User not found';
+  try {
+    //Confirm logged in user exist
+    const user = await findUserByEmail(email);
+    if (!user) throw 'User not found';
 
-        // confirm the user is either a tenant or landlord
-        if (!user.accountTypes.includes(accountType)) throw `User is not a ${+accountType === 1 ? 'tenant' : 'landlord'}`;
-        const transactions = await findTransaction(user.id, accountType);
+    // confirm the user is either a tenant or landlord
+    if (!user.accountTypes.includes(accountType)) throw `User is not a ${+accountType === 1 ? 'tenant' : 'landlord'}`;
+    const transactions = await findTransaction(user.id, accountType);
 
-        return apiResponse(res, `Tenants successfully fetched`, transactions, 201);
-    } catch (err) {
-        next(err);
-    }
+    return apiResponse(res, `Tenants successfully fetched`, transactions, 201);
+  } catch (err) {
+    next(err);
+  }
 }
-
--- server/src/modules/transaction/transaction.model.ts
+++ server/src/modules/transaction/transaction.model.ts
@@ -1,50 +1,45 @@
 import { getModelForClass, modelOptions, prop, Ref } from '@typegoose/typegoose';
 import { DocumentType, IModelOptions } from '@typegoose/typegoose/lib/types';
-import {User} from "../auth/register/register.model";
-import {Receipt} from "../receipt/receipt.model";
-
-
-
+import { User } from '../auth/register/register.model';
+import { Receipt } from '../receipt/receipt.model';
 
 const schemaOptions: IModelOptions = {
-    schemaOptions: {
-        toJSON: {
-            versionKey: false,
-            transform: function (doc: DocumentType<Transaction>, ret: any) {
-                ret.id = ret._id;
-                delete ret._id;
-            },
-        },
+  schemaOptions: {
+    toJSON: {
+      versionKey: false,
+      transform: function (doc: DocumentType<Transaction>, ret: any) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
     },
+  },
 };
 
 @modelOptions(schemaOptions)
 export class Transaction {
-    @prop({ ref: () => User })
-    public category!: Ref<Receipt>;
+  @prop({ required: true })
+  public category!: string;
 
-    @prop({ required: true })
-    public amount!: string;
+  @prop({ required: true })
+  public amount!: string;
 
-    @prop({ required: true, default: new Date() })
-    public dueDate!: Date;
+  @prop({ required: true, default: new Date() })
+  public dueDate!: Date;
 
-    @prop({ required: true })
-    public status?: string;
+  @prop({ required: true })
+  public status?: string;
 
-    @prop({ ref: () => User })
-    public created_by!: Ref<User>;
+  @prop({ ref: () => User })
+  public created_by!: Ref<User>;
 
-    @prop({ ref: () => User })
-    public received_by!: Ref<User>;
+  @prop({ ref: () => User })
+  public received_by!: Ref<User>;
 
-    @prop()
-    public transactionId?: string;
+  @prop()
+  public transactionId?: string;
 
-    @prop()
-    public receiptFile?: string;
+  @prop()
+  public receiptFile?: string;
 }
-
-
 
 export const TransactionModel = getModelForClass(Transaction, { schemaOptions: { timestamps: true } });
