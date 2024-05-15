7808e615df9d98f522a8c683296820093aef6b42
Oyeniji <nijoskysadeeq@gmail.com>
2024-05-15 09:46:36 +0100

    Invite tenant email

--- server/src/utils/email-templates/index.ts
+++ server/src/utils/email-templates/index.ts
@@ -56,10 +56,10 @@ export const emailTenantPropertyConfirmationLinkTemplate = (user: User, tenantIn
   return html;
 };
 
-export const inviteTenantLinkTemplate = (propertyId: string, invitedBy: string) => {
+export const inviteTenantLinkTemplate = (propertyId: string, invitedBy: string, userId: string) => {
   // get landlord name from property
-  
-  const inviteLink = `${CORS_ORIGIN}/auth/invite-tenant?propertyId=${propertyId}&invitedBy=${invitedBy}`;
+
+  const inviteLink = `${CORS_ORIGIN}/auth/invite-tenant?propertyId=${propertyId}&invitedBy=${invitedBy}&landlordId=${userId}`;
   //   const inviteLink = `${
   //     CORS_ORIGIN.split(',')[0] || CORS_ORIGIN.split(',')[2] ? CORS_ORIGIN.split(',')[2] : CORS_ORIGIN
   // }/auth/signup`;
