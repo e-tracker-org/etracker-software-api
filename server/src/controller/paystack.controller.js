52a0dc7e1ed129e4aecbb3bc763c648ba524442b
godfredakpan <godfredakpan@gmail.com>
2025-04-01 10:34:40 +0100

    updated backend User model

--- server/src/controller/paystack.controller.js
+++ server/src/controller/paystack.controller.js
@@ -218,9 +218,10 @@ router.post('/webhook', express.raw({ type: 'application/json' }), async (req, r
     const event = req.body;
 
     if (event.event === 'charge.success') {
-      const { email, amount, reference } = event.data;
+      const { amount, reference } = event.data;
       console.log("event", event);
       console.log("event data", event.data);
+      const email = event.data.customer.email;
       if (reference.startsWith('sub_')) {
         console.log('Updating subscription for user:', { email, reference });
         const updateResult = await UserModel.updateOne(
